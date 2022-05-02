884bb5356164da3deabcc048cc077556e336d79a
Gabor Szabo <gabor.attila.szabo@gmail.com>
2022-05-02 00:21:07 -0700

    Initial commit for multi-vari plot

---
+++ R/draw_multi_vari_chart.R
@@ -0,0 +1,19 @@
+#' Draw Multi-Vari Chart
+#'
+#' @description
+#' Draws a multi-vari small multiples plot
+#'
+#' @param
+#' @param
+#' @param
+#' @param
+#' @param
+#' @param
+#' @param
+#'
+#' @return A ggplot multi-vari small multiples object
+#'
+#' @export
+
+
+
