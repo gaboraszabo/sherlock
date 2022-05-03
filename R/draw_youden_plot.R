6452067eea795a4321076093b50be8eae809cd3b
Gabor Szabo <gabor.attila.szabo@gmail.com>
2022-05-02 23:48:35 -0700

    geom_point size 2

--- R/draw_youden_plot.R
+++ R/draw_youden_plot.R
@@ -61,7 +61,7 @@ draw_youden_plot <- function(data, x_axis_var, y_axis_var, lsl, usl,
     plot <- data %>%
 
       ggplot2::ggplot(ggplot2::aes(!!(meas_1_expr), !!(meas_2_expr))) +
-      ggplot2::geom_point(color  = "#3971CB", alpha  = 0.4, size = 2.5) +
+      ggplot2::geom_point(color  = "#3971CB", alpha  = 0.4, size = 2) +
 
       ggplot2::theme_light() +
       ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
@@ -95,7 +95,7 @@ draw_youden_plot <- function(data, x_axis_var, y_axis_var, lsl, usl,
       dplyr::mutate(!!grouping_var_expr := forcats::as_factor(!!grouping_var_expr)) %>%
 
       ggplot2::ggplot(ggplot2::aes(!!(meas_1_expr), !!(meas_2_expr))) +
-      ggplot2::geom_point(ggplot2::aes(color = !!(grouping_var_expr)), alpha  = 0.4, size = 2.5) +
+      ggplot2::geom_point(ggplot2::aes(color = !!(grouping_var_expr)), alpha  = 0.4, size = 2) +
 
       ggplot2::theme_light() +
       ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
