d7cd8c403c4a14a83d68a9017b1e3b0e774e5056
Gabor Szabo <gabor.attila.szabo@gmail.com>
2022-05-09 09:36:28 -0700

    initial commit

---
+++ R/scale_color_sherlock.R
@@ -0,0 +1,28 @@
+#' Sherlock Color Palettes
+#'
+#' @description
+#' Set color scheme to one of the Sherlock color palettes
+#'
+#' @param palette color palette to be used (required)
+
+#'
+#' @return Plot color scheme uses one of the Sherlock color palettes
+#'
+#' @export
+
+
+scale_color_sherlock <- function(palette = 1) {
+
+  if(palette == 1) {
+
+    ggplot2::scale_color_manual(values = c("#3971CB", "#D76213", "#111111", "#9A0000",
+                                           "#335F34", "#8E5816", "#624187", "#141B7A",
+                                           "#3971CB", "#D76213", "#111111", "#9A0000",
+                                           "#335F34", "#8E5816", "#624187", "#141B7A",
+                                           "#3971CB", "#D76213", "#111111", "#9A0000",
+                                           "#335F34", "#8E5816", "#624187", "#141B7A",
+                                           "#3971CB", "#D76213", "#111111", "#9A0000",
+                                           "#335F34", "#8E5816", "#624187", "#141B7A"))
+  }
+
+}
