b6d1dcb6279be2bbf3824e20df3142b151243451
Gabor Szabo <gabor.attila.szabo@gmail.com>
2022-05-10 05:34:45 -0700

    added theme elements

--- R/draw_polar_small_multiples.R
+++ R/draw_polar_small_multiples.R
@@ -29,7 +29,18 @@ draw_polar_small_multiples <- function(data, x, y, grouping_var, connect_with_li
     ggplot2::scale_x_continuous(breaks = c(0, 90, 180, 270),
                                 limits = c(0, 360)) +
     ggplot2::scale_y_continuous(limits = c(0, NA)) +
-    ggplot2::theme_light()
+    ggplot2::theme_light() +
+    ggplot2::theme(
+      panel.grid       = ggplot2::element_blank(),
+      strip.background = ggplot2::element_blank(),
+      strip.text       = ggplot2::element_text(color = "grey70", face = "bold", size = 11),
+      panel.border     = ggplot2::element_blank(),
+      panel.spacing    = ggplot2::unit(1, "lines"),
+      axis.text        = ggplot2::element_blank(),
+      axis.title       = ggplot2::element_blank(),
+      axis.ticks       = ggplot2::element_blank(),
+      legend.title     = ggplot2::element_text(color = "grey50", size = 11),
+      legend.text      = ggplot2::element_text(color = "grey50", size = 11))
 
   if (connect_with_lines) {
     plot <- plot + ggplot2::geom_line()
