14b56c67357dc4422cc331105f5e8c27fc022ea4
Gabor Szabo <gabor.attila.szabo@gmail.com>
2022-05-10 05:38:36 -0700

    changed theme elements

--- R/draw_polar_small_multiples.R
+++ R/draw_polar_small_multiples.R
@@ -31,16 +31,18 @@ draw_polar_small_multiples <- function(data, x, y, grouping_var, connect_with_li
     ggplot2::scale_y_continuous(limits = c(0, NA)) +
     ggplot2::theme_light() +
     ggplot2::theme(
-      panel.grid       = ggplot2::element_blank(),
       strip.background = ggplot2::element_blank(),
       strip.text       = ggplot2::element_text(color = "grey70", face = "bold", size = 11),
       panel.border     = ggplot2::element_blank(),
       panel.spacing    = ggplot2::unit(1, "lines"),
-      axis.text        = ggplot2::element_blank(),
-      axis.title       = ggplot2::element_blank(),
-      axis.ticks       = ggplot2::element_blank(),
+      axis.title.x     = ggplot2::element_text(size = 11, color = "grey50"),
+      axis.title.y     = ggplot2::element_text(size = 11, color = "grey50"),
+      axis.text        = ggplot2::element_text(size = 11, color = "grey50"),
       legend.title     = ggplot2::element_text(color = "grey50", size = 11),
-      legend.text      = ggplot2::element_text(color = "grey50", size = 11))
+      legend.text      = ggplot2::element_text(color = "grey50", size = 11),
+      plot.title       = ggplot2::element_text(color = "grey50", size = 14, hjust = 0),
+      plot.subtitle    = ggplot2::element_text(color = "grey50", size = 11, hjust = 0),
+      plot.caption     = ggplot2::element_text(color = "grey50", size = 8))
 
   if (connect_with_lines) {
     plot <- plot + ggplot2::geom_line()
