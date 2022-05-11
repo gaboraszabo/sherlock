d7693bd42ab3945700aa5d15808c21ef694cfe6a
Gabor Szabo <gabor.attila.szabo@gmail.com>
2022-05-10 22:34:25 -0700

    commit: first fix

--- R/draw_multi_vari_plot.R
+++ R/draw_multi_vari_plot.R
@@ -72,14 +72,14 @@ draw_multi_vari_plot <- function(data, response, factor_1, factor_2, factor_3, p
     multi_vari_chart <- multi_vari_tbl %>%
 
       ggplot2::ggplot(ggplot2::aes(!!factor_1_expr, !!response_expr, color = !!factor_1_expr)) +
-      ggh4x::facet_nested(rows = vars(), cols = vars(!!factor_2_expr)) +
+      ggh4x::facet_nested(rows = ggplot2::vars(), cols = ggplot2::vars(!!factor_2_expr)) +
       ggplot2::guides(y.sec = "axis") +
-      ggplot2::theme_element +
+      theme_element +
       ggplot2::labs(
         title = "Multi-Vari Chart",
         subtitle = str_glue("{as_label(factor_1_expr)} by {as_label(factor_2_expr)}"),
-        x = str_glue("{as_label(factor_1_expr)}"),
-        y = str_glue("{as_label(response_expr)}")
+        x = stringr::str_glue("{as_label(factor_1_expr)}"),
+        y = stringr::str_glue("{as_label(response_expr)}")
       )
 
     if (plot_means) {
@@ -87,7 +87,7 @@ draw_multi_vari_plot <- function(data, response, factor_1, factor_2, factor_3, p
         ggplot2::geom_point(color = "grey30", size = 1.5, alpha = 0.4) +
         ggplot2::geom_point(data = mean_tbl, aes(!!factor_1_expr, mean), color = "#40506e", size = 3, alpha = 0.7) +
         ggplot2::geom_line(data = mean_tbl, aes(!!factor_1_expr, mean, group = 1), color = "#40506e", alpha = 0.7) +
-        ggplot2::labs(caption  = str_glue("Blue data points represent averages for factor {as_label(factor_1_expr)}"))
+        ggplot2::labs(caption  = stringr::str_glue("Blue data points represent averages for factor {as_label(factor_1_expr)}"))
     } else {
       multi_vari_chart <- multi_vari_chart +
         ggplot2::geom_point(size = 2.5, alpha = 0.6) +
@@ -121,17 +121,17 @@ draw_multi_vari_plot <- function(data, response, factor_1, factor_2, factor_3, p
         ggplot2::geom_point(data = mean_tbl, aes(!!factor_2_expr, mean), color = "#40506e", size = 3, alpha = 0.7) +
         ggplot2::geom_line(data = mean_tbl, aes(!!factor_2_expr, mean, group = 1), color = "#40506e", alpha = 0.7) +
 
-        ggh4x::facet_nested(rows = vars(), cols = vars(!!factor_3_expr)) +
+        ggh4x::facet_nested(rows = ggplot2::vars(), cols = ggplot2::vars(!!factor_3_expr)) +
         ggplot2::guides(y.sec = "axis") +
 
         theme_element +
         ggplot2::labs(
           title    = "Multi-Vari Chart",
-          subtitle = str_glue("{as_label(factor_1_expr)} by {as_label(factor_2_expr)} by {as_label(factor_3_expr)}"),
-          x        = str_glue("{as_label(factor_2_expr)}"),
-          y        = str_glue("{as_label(response_expr)}"),
-          caption  = str_glue("Blue data points represent averages for factor {as_label(factor_2_expr) }
-                              Grey data points represent individual values for factor {as_label(factor_1_expr)}")
+          subtitle = stringr::str_glue("{as_label(factor_1_expr)} by {as_label(factor_2_expr)} by {as_label(factor_3_expr)}"),
+          x        = stringr::str_glue("{as_label(factor_2_expr)}"),
+          y        = stringr::str_glue("{as_label(response_expr)}"),
+          caption  = stringr::str_glue("Blue data points represent averages for factor {as_label(factor_2_expr) }
+                                       Grey data points represent individual values for factor {as_label(factor_1_expr)}")
         )
 
     } else {
@@ -142,15 +142,15 @@ draw_multi_vari_plot <- function(data, response, factor_1, factor_2, factor_3, p
         ggplot2::geom_point(size = 2.5, alpha = 0.6) +
         ggplot2::geom_line(size = 0.7, group = 1, alpha = 0.6) +
 
-        ggh4x::facet_nested(rows = vars(), cols = vars(!!factor_3_expr, !!factor_2_expr)) +
+        ggh4x::facet_nested(rows = ggplot2::vars(), cols = ggplot2::vars(!!factor_3_expr, !!factor_2_expr)) +
         ggplot2::guides(y.sec = "axis") +
 
         theme_element +
         ggplot2::labs(
           title    = "Multi-Vari Chart",
-          subtitle = str_glue("{as_label(factor_1_expr)} by {as_label(factor_2_expr)} by {as_label(factor_3_expr)}"),
-          x        = str_glue("{as_label(factor_1_expr)}"),
-          y        = str_glue("{as_label(response_expr)}")
+          subtitle = stringr::str_glue("{as_label(factor_1_expr)} by {as_label(factor_2_expr)} by {as_label(factor_3_expr)}"),
+          x        = stringr::str_glue("{as_label(factor_1_expr)}"),
+          y        = stringr::str_glue("{as_label(response_expr)}")
         )
 
     }
