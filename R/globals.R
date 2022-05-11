cc1c822b2c701eb9336c12dff737277afc1d7630
Gabor Szabo <gabor.attila.szabo@gmail.com>
2022-05-11 10:32:41 -0700

    new function: add_horizontal_reference_line added multi_vari_data dataset and .R updated globals

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,4 @@
 utils::globalVariables(
   c("range_min", "range_max", "range_min_2", "range_max_2", "measurement_1", "measurement_2", "location", "theme_element",
-    "mean_tbl", "multi_vari_tbl", "multi_vari_data_2", "Length", "Trial", "Part", "Operator"))
+    "mean_tbl", "multi_vari_tbl", "multi_vari_data_2", "Length", "Trial", "Part", "Operator", "multi_vari_data",
+    "cycle", "fixture", "line", "force"))
