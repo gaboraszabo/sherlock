f395360e4255e17c9afb710337f479ce092cefdb
Gabor Szabo <gabor.attila.szabo@gmail.com>
2022-05-16 07:05:08 -0700

    added size argument

--- R/add_horizontal_reference_line.R
+++ R/add_horizontal_reference_line.R
@@ -6,6 +6,7 @@
 #' @param reference_line input y coordinate of reference line(s). for multiple reference lines, concatenate individual values into a vector  (required)
 #' @param color change reference line color. options are "grey", "blue" and "red". by default, it is set to "grey" (optional)
 #' @param linetype change line type. identical to linetype ggplot2 aesthetic. by default, it is set to "dashed" (optional)
+#' @param size change line thickness. identical to size ggplot2 aesthetic. by default, it is set to 0.7 (optional)
 #'
 #' @return A ggplot multi-vari plot object
 #'
@@ -18,18 +19,18 @@
 #'
 #' @export
 
-add_horizontal_reference_line <- function(reference_line, color = "grey", linetype = "dashed") {
+add_horizontal_reference_line <- function(reference_line, color = "grey", linetype = "dashed", size = 0.7) {
 
   if(color == "grey") {
-    ref_line <- ggplot2::geom_hline(yintercept = reference_line, color = "grey50", linetype = linetype, alpha = 0.5, size = 1)
+    ref_line <- ggplot2::geom_hline(yintercept = reference_line, color = "grey50", linetype = linetype, alpha = 0.5, size = size)
   }
 
   if(color == "blue") {
-    ref_line <- ggplot2::geom_hline(yintercept = reference_line, color = "#40506e", linetype = linetype, alpha = 0.7, size = 1)
+    ref_line <- ggplot2::geom_hline(yintercept = reference_line, color = "#40506e", linetype = linetype, alpha = 0.7, size = size)
   }
 
   if(color == "red") {
-    ref_line <- ggplot2::geom_hline(yintercept = reference_line, color = "#700808", linetype = linetype, alpha = 0.5, size = 1)
+    ref_line <- ggplot2::geom_hline(yintercept = reference_line, color = "#700808", linetype = linetype, alpha = 0.5, size = size)
   }
 
   return(ref_line)
