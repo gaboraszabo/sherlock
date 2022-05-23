fe240631d7b4d900a933f42c99c14791be512a1f
Gabor Szabo <gabor.attila.szabo@gmail.com>
2022-05-22 22:02:59 -0700

    changed name of function from add_horizontal_reference_line() to draw_horizontal_reference_line()

--- R/draw_horizontal_reference_line.R
+++ R/draw_horizontal_reference_line.R
@@ -1,7 +1,7 @@
-#' Add horizontal reference line
+#' Draw horizontal reference line
 #'
 #' @description
-#' Adds horizontal reference line(s) to plots
+#' Draws horizontal reference line(s) to plots
 #'
 #' @param reference_line input y coordinate of reference line(s). for multiple reference lines, concatenate individual values into a vector  (required)
 #' @param color change reference line color. options are "grey", "blue" and "red". by default, it is set to "grey" (optional)
@@ -19,7 +19,7 @@
 #'
 #' @export
 
-add_horizontal_reference_line <- function(reference_line, color = "grey", linetype = "dashed", size = 0.5) {
+draw_horizontal_reference_line <- function(reference_line, color = "grey", linetype = "dashed", size = 0.5) {
 
   if(color == "grey") {
     ref_line <- ggplot2::geom_hline(yintercept = reference_line, color = "grey50", linetype = linetype, alpha = 0.5, size = size)
