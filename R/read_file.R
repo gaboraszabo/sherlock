67072dc17a32918a9391bf3db3a52cc69f2722df
Gabor Szabo <gabor.attila.szabo@gmail.com>
2022-06-06 23:55:03 -0700

    new function read_file() updated description file

---
+++ R/read_file.R
@@ -0,0 +1,28 @@
+#' Read File
+#'
+#' @description
+#' Reads either an .xlsx or a .csv file into a table
+#'
+#' @param path path for the file (required)
+#' @param filetype set whether to read an .xlsx file or a .csv file. It takes either ".xlsx" or ".csv". By default, it is set to ".xlsx" (optional)
+#'
+#' @return A tibble
+#'
+#' @export
+
+read_file <- function(path, filetype = ".xlsx") {
+
+  if (filetype == ".xlsx") {
+    data <- openxlsx::read.xlsx(xlsxFile = path) %>% as_tibble()
+  }
+
+  if (filetype == ".csv") {
+    data <-  readr::read_csv(file = path) %>% as_tibble()
+  }
+
+
+  if (filetype != ".xlsx" || filetype != ".csv") {
+    warning("Wrong filetype entered. You need to enter either '.xlsx' or '.csv'.")
+  }
+
+}
