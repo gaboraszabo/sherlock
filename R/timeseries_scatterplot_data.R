ef02fe315a397f228661b13ef13fdc23f103e86d
Gabor Szabo <gabor.attila.szabo@gmail.com>
2022-07-11 22:27:39 -0700

    added timeseries_scatterplot_data sample dataset created data file created .r file updated globals

---
+++ R/timeseries_scatterplot_data.R
@@ -0,0 +1,9 @@
+#' Timeseries Scatterplot Sample Dataset
+#'
+#'Contains a sample Timerseries Scatterplot dataset
+#'
+#' @examples
+#' timeseries_scatterplot_data
+#'
+
+"timeseries_scatterplot_data"
