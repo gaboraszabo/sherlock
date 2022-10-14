49643f5fba63c1ee92be57dc8e0561a7f602cbcb
Gabor Szabo <gabor.attila.szabo@gmail.com>
2022-10-14 07:27:02 -0700

    interaction plot: added point_size and line_size args

--- R/draw_interaction_plot.R
+++ R/draw_interaction_plot.R
@@ -7,6 +7,8 @@
 #' @param y_var Y variable to be plotted on Y axis (required)
 #' @param x_var_1_levels First grouping variable levels, e.g. -1/1 or "low"/"high" (required)
 #' @param x_var_2_levels Second grouping variable levels, e.g. -1/1 or "low"/"high" (required)
+#' @param point_size Set point size. By default, it is set to 4  (optional)
+#' @param line_size Set line size. By default, it is set to 1  (optional)
 #' @param alpha Set transparency. By default, it is set to 0.5  (optional)
 #' @param analysis_desc_label analysis_desc_label Label (subtitle) for analysis description. By default, it is set to NULL  (optional)
 #'
@@ -14,7 +16,8 @@
 #'
 #' @export
 
-draw_interaction_plot <- function(data, y_var, x_var_1_levels, x_var_2_levels, alpha = 0.5, analysis_desc_label = NULL) {
+draw_interaction_plot <- function(data, y_var, x_var_1_levels, x_var_2_levels, point_size = 4, line_size = 1,
+                                  alpha = 0.5, analysis_desc_label = NULL) {
 
   # 1. TIDY EVAL
   y_var_expr          <- rlang::enquo(y_var)
@@ -29,8 +32,8 @@ draw_interaction_plot <- function(data, y_var, x_var_1_levels, x_var_2_levels, a
   # 3. PLOTTING FUNCTION
   plot <- data %>%
     ggplot2::ggplot(ggplot2::aes(x = !!x_var_1_expr, y = !!y_var_expr, color = !!x_var_2_expr)) +
-    ggplot2::geom_point(alpha = alpha, size = 4, shape = 21, stroke = 1.2) +
-    ggplot2::stat_summary(ggplot2::aes(y = !!y_var_expr, group = !!x_var_2_expr), fun = mean, size = 1, geom = "line", alpha = alpha) +
+    ggplot2::geom_point(alpha = alpha, size = point_size, shape = 21, stroke = 1.2) +
+    ggplot2::stat_summary(ggplot2::aes(y = !!y_var_expr, group = !!x_var_2_expr), fun = mean, size = line_size, geom = "line", alpha = alpha) +
     sherlock::theme_sherlock() +
     sherlock::scale_color_sherlock() +
     ggplot2::labs(title = "Interaction Plot", subtitle = analysis_desc_label)
