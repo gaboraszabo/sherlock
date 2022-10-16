76a42d5a0a6ddddc072391be710a976f9481ffb0
Gabor Szabo <gabor.attila.szabo@gmail.com>
2022-10-16 15:44:33 -0700

    created polar_small_multiples_data sample dataset

---
+++ R/polar_small_multiples_data.R
@@ -0,0 +1,9 @@
+#' Polar Small Multiples Sample Dataset
+#'
+#'Contains a sample dataset to demonstrate the use of Polar Small Multiples plot
+#'
+#' @examples
+#' polar_small_multiples_data
+#'
+
+"polar_small_multiples_data"
