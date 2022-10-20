cb4c623ca49e6275f370bd3d8442145701a44255
Gabor Szabo <gabor.attila.szabo@gmail.com>
2022-10-20 00:06:27 -0700

    explicit

--- R/draw_pareto_chart.R
+++ R/draw_pareto_chart.R
@@ -11,7 +11,6 @@
 #' @param lumped_cat_name  (optional)
 #' @param column_fill  (optional)
 #' @param scale  (optional)
-#' @param alpha  (optional)
 #' @param axis_text_size (optional)
 #'
 #' @return A Pareto Chart ggplot object
@@ -26,49 +25,49 @@ draw_pareto_chart <- function(data, cat_var, y_var, highlight_first_n_items = 3,
   y_var_expr <- rlang::enquo(y_var)
 
   data <- data %>%
-    arrange(desc(!!y_var_expr)) %>%
-    mutate(rank = row_number()) %>%
-    mutate(!!(cat_var_expr)   := !!(cat_var_expr) %>% as_factor() %>% fct_rev()) %>%
-    mutate(!!(y_var_expr) := !!(y_var_expr) %>% as.numeric()) %>%
-    mutate(fill = column_fill) %>%
-    mutate(alpha = case_when(rank <= highlight_first_n_items ~ 0.8,
-                             TRUE ~ 0.4))
+    dplyr::arrange(desc(!!y_var_expr)) %>%
+    dplyr::mutate(rank = row_number()) %>%
+    dplyr::mutate(!!(cat_var_expr)   := !!(cat_var_expr) %>% forcats::as_factor() %>% forcats::fct_rev()) %>%
+    dplyr::mutate(!!(y_var_expr) := !!(y_var_expr) %>% as.numeric()) %>%
+    dplyr::mutate(fill = column_fill) %>%
+    dplyr::mutate(alpha = dplyr::case_when(rank <= highlight_first_n_items ~ 0.8,
+                                           TRUE ~ 0.4))
 
   if (lump_last_n_items > 0) {
     data <- data %>%
-      mutate(!!(cat_var_expr) := !!(cat_var_expr) %>% fct_lump(n = (data %>% pull(!!(cat_var_expr)) %>% length()) - lump_last_n_items,
-                                                               w = !!(y_var_expr),
-                                                               other_level = lumped_cat_name)) %>%
-      mutate(!!(cat_var_expr) := !!(cat_var_expr) %>% fct_reorder(!!(y_var_expr))) %>%
-      mutate(!!(cat_var_expr) := !!(cat_var_expr) %>% fct_relevel(lumped_cat_name, after = 0))
+      dplyr::mutate(!!(cat_var_expr) := !!(cat_var_expr) %>% forcats::fct_lump(n = (data %>% pull(!!(cat_var_expr)) %>% length()) - lump_last_n_items,
+                                                                               w = !!(y_var_expr),
+                                                                               other_level = lumped_cat_name)) %>%
+      dplyr::mutate(!!(cat_var_expr) := !!(cat_var_expr) %>% forcats::fct_reorder(!!(y_var_expr))) %>%
+      dplyr::mutate(!!(cat_var_expr) := !!(cat_var_expr) %>% forcats::fct_relevel(lumped_cat_name, after = 0))
   }
 
   plot <- data %>%
-    ggplot(aes(!!(cat_var_expr), !!(y_var_expr))) +
-    geom_col(fill = column_fill, alpha = data$alpha) +
-    coord_flip() +
-    theme_sherlock() +
-    theme(axis.title.x = element_blank(),
-          plot.title   = element_text(size = 18),
-          axis.text.x  = element_text(size = axis_text_size)) +
-    scale_y_continuous(position = "right") +
-    labs(title    = "Pareto Chart",
-         subtitle = str_glue("{as_label(y_var_expr)} by {as_label(cat_var_expr)}"))
+    ggplot2::ggplot(ggplot2::aes(!!(cat_var_expr), !!(y_var_expr))) +
+    ggplot2::geom_col(fill = column_fill, alpha = data$alpha) +
+    ggplot2::coord_flip() +
+    sherlock::theme_sherlock() +
+    ggplot2::theme(axis.title.x = ggplot2::element_blank(),
+                   plot.title   = ggplot2::element_text(size = 18),
+                   axis.text.x  = ggplot2::element_text(size = axis_text_size)) +
+    ggplot2::scale_y_continuous(position = "right") +
+    ggplot2::labs(title    = "Pareto Chart",
+                  subtitle = stringr::str_glue("{as_label(y_var_expr)} by {as_label(cat_var_expr)}"))
 
   if (scale == "percent") {
-    plot <- plot + scale_y_continuous(position = "right", labels = scales::percent_format(accuracy = 0.1))
+    plot <- plot + ggplot2::scale_y_continuous(position = "right", labels = scales::percent_format(accuracy = 0.1))
   }
 
   if (scale == "dollars") {
-    plot <- plot + scale_y_continuous(position = "right", labels = scales::dollar_format())
+    plot <- plot + ggplot2::scale_y_continuous(position = "right", labels = scales::dollar_format())
   }
 
   if (scale == "dollars-k") {
-    plot <- plot + scale_y_continuous(position = "right", labels = scales::dollar_format(scale = 0.001, suffix = "k", accuracy = 1))
+    plot <- plot + ggplot2::scale_y_continuous(position = "right", labels = scales::dollar_format(scale = 0.001, suffix = "k", accuracy = 1))
   }
 
   if (scale == "dollars-M") {
-    plot <- plot + scale_y_continuous(position = "right", labels = scales::dollar_format(scale = 0.000001, suffix = "M", accuracy = 0.1))
+    plot <- plot + ggplot2::scale_y_continuous(position = "right", labels = scales::dollar_format(scale = 0.000001, suffix = "M", accuracy = 0.1))
   }
 
 
