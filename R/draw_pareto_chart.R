fd8529492f85e399a94b3a2947b3466cda0ae36d
Gabor Szabo <gabor.attila.szabo@gmail.com>
2022-11-01 04:55:06 -0700

    changes

--- R/draw_pareto_chart.R
+++ R/draw_pareto_chart.R
@@ -11,6 +11,8 @@
 #' @param lumped_cat_name  (optional)
 #' @param column_fill  (optional)
 #' @param scale  (optional)
+#' @param title_label  (optional)
+#' @param analysis_desc_label  (optional)
 #' @param axis_text_size (optional)
 #'
 #' @return A Pareto Chart ggplot object
@@ -18,7 +20,8 @@
 #' @export
 
 draw_pareto_chart <- function(data, cat_var, y_var, highlight_first_n_items = 0, lump_last_n_items = 0,
-                              lumped_cat_name = "Other", column_fill = scale_fill_sherlock(3), scale = "numeric", axis_text_size = 10) {
+                              lumped_cat_name = "Other", column_fill = scale_fill_sherlock(3), scale = "numeric",
+                              title_label = "Pareto Chart", analysis_desc_label = NULL, axis_text_size = 10) {
 
 
   cat_var_expr   <- rlang::enquo(cat_var)
@@ -27,7 +30,7 @@ draw_pareto_chart <- function(data, cat_var, y_var, highlight_first_n_items = 0,
   data <- data %>%
     dplyr::arrange(dplyr::desc(!!y_var_expr)) %>%
     dplyr::mutate(rank = dplyr::row_number()) %>%
-    dplyr::mutate(!!(cat_var_expr)   := !!(cat_var_expr) %>% forcats::as_factor() %>% forcats::fct_rev()) %>%
+    dplyr::mutate(!!(cat_var_expr) := !!(cat_var_expr) %>% forcats::as_factor() %>% forcats::fct_rev()) %>%
     dplyr::mutate(!!(y_var_expr) := !!(y_var_expr) %>% as.numeric()) %>%
     dplyr::mutate(fill = column_fill) %>%
     dplyr::mutate(alpha = dplyr::case_when(rank <= highlight_first_n_items ~ 0.8,
@@ -51,22 +54,24 @@ draw_pareto_chart <- function(data, cat_var, y_var, highlight_first_n_items = 0,
                    plot.title   = ggplot2::element_text(size = 18),
                    axis.text.x  = ggplot2::element_text(size = axis_text_size)) +
     ggplot2::scale_y_continuous(position = "right") +
-    ggplot2::labs(title    = "Pareto Chart",
-                  subtitle = stringr::str_glue("{as_label(y_var_expr)} by {as_label(cat_var_expr)}"))
+    ggplot2::labs(title    = title_label,
+                  subtitle = ifelse(is.null(analysis_desc_label),
+                                    stringr::str_glue("{as_label(y_var_expr)} by {as_label(cat_var_expr)}"),
+                                    analysis_desc_label))
 
   if (scale == "percent") {
     plot <- plot + ggplot2::scale_y_continuous(position = "right", labels = scales::percent_format(accuracy = 0.1))
   }
 
-  if (scale == "dollars") {
+  if (scale == "dollar") {
     plot <- plot + ggplot2::scale_y_continuous(position = "right", labels = scales::dollar_format())
   }
 
-  if (scale == "dollars-k") {
+  if (scale == "dollar-k") {
     plot <- plot + ggplot2::scale_y_continuous(position = "right", labels = scales::dollar_format(scale = 0.001, suffix = "k", accuracy = 1))
   }
 
-  if (scale == "dollars-M") {
+  if (scale == "dollar-M") {
     plot <- plot + ggplot2::scale_y_continuous(position = "right", labels = scales::dollar_format(scale = 0.000001, suffix = "M", accuracy = 0.1))
   }
 
