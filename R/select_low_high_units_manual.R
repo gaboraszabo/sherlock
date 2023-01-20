22883805238fe89a17cf1c60c2f2b54203205f5a
Gabor Szabo <gabor.attila.szabo@gmail.com>
2023-01-20 12:02:29 -0800

    select_low_high_units_manual() function

---
+++ R/select_low_high_units_manual.R
@@ -0,0 +1,71 @@
+#' Select Low-High Units
+#'
+#' @description
+#' Select low-high units manually in a tibble and assign them into groups
+#'
+#' @param data input dataset (required)
+#' @param select_units_by Set to select units either based on row number or part ID. Options are "row_number" and "part_id". By default, it is set to "row_number". (required)
+#' @param lowest_units A numerical or character vector of the lowest units selected. Examples: c(1, 6, 8, 12), c("part5", "part45", "part9", "part23"). (required)
+#' @param highest_units A numerical or character vector of the lowest units selected. Examples: c(1, 6, 8, 12), c("part5", "part45", "part9", "part23"). (required)
+#' @param part_id_col Set column for part id. Only to be used when select_units_by is set to "part_id".
+#'
+#' @return A tibble object filtered down to the low-high units selected
+#'
+#' @export
+
+select_low_high_units_manual <- function(data, select_units_by = "row_number", lowest_units, highest_units, part_id_col) {
+
+  # Tidy Eval ----
+  part_id_col_expr <- enquo(part_id_col)
+
+
+  # Data Transformation ----
+  if (select_units_by == "row_number") {
+
+    lowest_units <- data %>%
+      slice(lowest_units) %>%
+      mutate(low_high = "low") %>%
+      mutate(color_label = "red")
+
+    highest_units <- data %>%
+      slice(highest_units) %>%
+      mutate(low_high = "high") %>%
+      mutate(color_label = "black")
+  }
+
+  if (select_units_by == "part_id") {
+
+    data <- data %>%
+      mutate(!!part_id_col_expr := !!part_id_col_expr %>% as.integer())
+
+    lowest_units <- data %>%
+      filter(!!part_id_col_expr %in% lowest_units) %>%
+      mutate(low_high = "low") %>%
+      mutate(color_label = "red")
+
+    highest_units <- data %>%
+      filter(!!part_id_col_expr %in% highest_units) %>%
+      mutate(low_high = "high") %>%
+      mutate(color_label = "black")
+  }
+
+
+  low_high_tbl <- bind_rows(highest_units, lowest_units)
+
+  low_high_tbl <- low_high_tbl %>%
+    group_by(low_high) %>%
+    mutate(Pair = 1:n()) %>%
+    ungroup()
+
+  # REARRANGE COLUMNS ----
+  if (missing(part_id_col)) {
+    low_high_tbl <- low_high_tbl %>%
+      select(Pair, everything())
+  } else {
+    low_high_tbl <- low_high_tbl %>%
+      select(Pair, !!part_id_col_expr, everything())
+  }
+
+
+  return(low_high_tbl)
+}
