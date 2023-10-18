96e2c79562f67f11100d2fc16760ae9821076be3
Gabor Szabo <gabor.attila.szabo@gmail.com>
2023-10-18 05:42:03 -0700

    new function create_process_behavior_chart_limits()

---
+++ R/create_process_behavior_chart_limits.R
@@ -0,0 +1,39 @@
+#' Create Process Behavior Chart Limits
+#'
+#' @description
+#' Creates limits (LCL, UCL) and related calculations (moving range, LCL, UCL) for a Process Behavior Chart and stores them in separate columns
+#'
+#' @param data input dataset to be plotted (required)
+#' @param y_var Y variable to be plotted on Y axis (required)
+#' @param grouping_var Variable to group by (optional)
+#'
+#' @return A tibble with columns created to store values for the moving range, average, UCL and LCL
+#'
+#' @export
+
+
+create_process_behavior_chart_limits <- function(data, y_var, grouping_var) {
+
+  y_var_expr        <- rlang::enquo(y_var)
+  grouping_var_expr <- rlang::enquo(grouping_var)
+
+  if (missing(grouping_var)) {
+    data <- data %>%
+      dplyr::mutate(Average = mean(!!y_var_expr)) %>%
+      dplyr::mutate(moving_range = abs(dplyr::lag(!!y_var_expr) - !!y_var_expr)) %>%
+      dplyr::mutate(lcl = mean(Average) - 2.66 * mean(moving_range, na.rm = TRUE)) %>%
+      dplyr::mutate(ucl = mean(Average) + 2.66 * mean(moving_range, na.rm = TRUE))
+  }
+
+  if (!missing(grouping_var)) {
+    data <- data %>%
+      dplyr::group_by(!!grouping_var_expr) %>%
+      dplyr::mutate(Average = mean(!!y_var_expr)) %>%
+      dplyr::mutate(moving_range = abs(dplyr::lag(!!y_var_expr) - !!y_var_expr)) %>%
+      dplyr::mutate(lcl = mean(Average) - 2.66 * mean(moving_range, na.rm = TRUE)) %>%
+      dplyr::mutate(ucl = mean(Average) + 2.66 * mean(moving_range, na.rm = TRUE)) %>%
+      dplyr::ungroup()
+  }
+
+  return(data)
+}
