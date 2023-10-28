58226c422f81414c1ea20180f1cd83fe50b684f1
Gabor Szabo <gabor.attila.szabo@gmail.com>
2023-10-28 14:17:43 -0700

    added across for selecting multiple columns to group by

--- R/create_process_behavior_chart_limits.R
+++ R/create_process_behavior_chart_limits.R
@@ -5,19 +5,18 @@
 #'
 #' @param data input dataset to be plotted (required)
 #' @param y_var Y variable to be plotted on Y axis (required)
-#' @param grouping_var Variable to group by (optional)
+#' @param grouping_vars Variable(s) to group by (optional)
 #'
 #' @return A tibble with columns created to store values for the moving range, average, UCL and LCL
 #'
 #' @export
 
 
-create_process_behavior_chart_limits <- function(data, y_var, grouping_var) {
+create_process_behavior_chart_limits <- function(data, y_var, grouping_vars) {
 
   y_var_expr        <- rlang::enquo(y_var)
-  grouping_var_expr <- rlang::enquo(grouping_var)
 
-  if (missing(grouping_var)) {
+  if (missing(grouping_vars)) {
     data <- data %>%
       dplyr::mutate(Average = mean(!!y_var_expr)) %>%
       dplyr::mutate(Moving_Range = abs(dplyr::lag(!!y_var_expr) - !!y_var_expr)) %>%
@@ -25,9 +24,9 @@ create_process_behavior_chart_limits <- function(data, y_var, grouping_var) {
       dplyr::mutate(UCL = mean(Average) + 2.66 * mean(Moving_Range, na.rm = TRUE))
   }
 
-  if (!missing(grouping_var)) {
+  if (!missing(grouping_vars)) {
     data <- data %>%
-      dplyr::group_by(!!grouping_var_expr) %>%
+      dplyr::group_by(across({{ grouping_vars }})) %>%
       dplyr::mutate(Average = mean(!!y_var_expr)) %>%
       dplyr::mutate(Moving_Range = abs(dplyr::lag(!!y_var_expr) - !!y_var_expr)) %>%
       dplyr::mutate(LCL = mean(Average) - 2.66 * mean(Moving_Range, na.rm = TRUE)) %>%
